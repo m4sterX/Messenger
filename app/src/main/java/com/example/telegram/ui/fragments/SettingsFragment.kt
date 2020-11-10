a33c5246b45499927e3333d41b092e72864fdbe0
nkttrvts <3119009@gmail.com>
2020-11-10 23:59:09 +0300

    settings ui

--- app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.telegram.R
 
 
@@ -7,5 +9,11 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)
     }
 }
